0eb7468f38455d68a74b4dca6c31894f35548aab
zakharovaleriia <zaharova.vs@edu.spbstu.ru>
2026-03-03 14:35:29 +0300

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,13 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+index_of_skipped_item=numbers.index(None) #индекс пропущенного элемента
+
+numbers_copy=numbers.copy() #копия списка для подсчета суммы
+numbers_copy.pop(index_of_skipped_item) #список без пропущенного элемента
+
+average=sum(numbers_copy)/len(numbers) #среднее арифметическое
+
+numbers[index_of_skipped_item]=average #замена пропущенного элемента на ср. арифм.
+
+print("Измененный список:", numbers)
+
---
+++ task 2.py
@@ -0,0 +1,20 @@
+
+#исходные данные
+
+card_storage=1.44 #объем дискеты в Мб
+card_storage_bites=1.44*1024*1024 #объем дискеты в байтах
+pages_per_book=100 #количество страниц в одной книге
+strings_per_page=50 #количество строк на одной странице
+symbols_per_string=25 #количество символов на одной строке
+bites_per_symbol=4 #количество байт, занимаемых одним символом
+
+string_storage=bites_per_symbol*symbols_per_string #объём, занимаемый одной строкой
+
+page_storage=strings_per_page*string_storage #объём, занимаемый одной страницей
+
+book_storage=pages_per_book*page_storage #объём, занимаемый одной книгой
+
+books_per_card=int(card_storage_bites//book_storage) #количество книг, которое можно разместить на дискете
+
+print("Количество книг, помещающихся на дискету:", books_per_card)
+
