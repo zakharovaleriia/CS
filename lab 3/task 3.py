54c731494a2ef67acb6b84ccba6c261f5ebce224
zakharovaleriia <zaharova.vs@edu.spbstu.ru>
2026-03-28 22:47:40 +0300

    Add files via upload

---
+++ lab 3/task 3.py
@@ -0,0 +1,60 @@
+# TODO  Напишите функцию count_letters
+
+
+# TODO Напишите функцию calculate_frequency
+
+
+main_str = """
+У лукоморья дуб зелёный;
+Златая цепь на дубе том:
+И днём и ночью кот учёный
+Всё ходит по цепи кругом;
+Идёт направо — песнь заводит,
+Налево — сказку говорит.
+Там чудеса: там леший бродит,
+Русалка на ветвях сидит;
+Там на неведомых дорожках
+Следы невиданных зверей;
+Избушка там на курьих ножках
+Стоит без окон, без дверей;
+Там лес и дол видений полны;
+Там о заре прихлынут волны
+На брег песчаный и пустой,
+И тридцать витязей прекрасных
+Чредой из вод выходят ясных,
+И с ними дядька их морской;
+Там королевич мимоходом
+Пленяет грозного царя;
+Там в облаках перед народом
+Через леса, через моря
+Колдун несёт богатыря;
+В темнице там царевна тужит,
+А бурый волк ей верно служит;
+Там ступа с Бабою Ягой
+Идёт, бредёт сама собой,
+Там царь Кащей над златом чахнет;
+Там русский дух… там Русью пахнет!
+И там я был, и мёд я пил;
+У моря видел дуб зелёный;
+Под ним сидел, и кот учёный
+Свои мне сказки говорил.
+"""
+
+
+
+all_letters = [x.lower() for x in list(main_str) if x.isalpha() == True]
+unique_letters = list(dict.fromkeys(all_letters))
+
+quantity_dictionary=dict()
+for letter in unique_letters:
+    quantity_dictionary[letter] = all_letters.count(letter)
+
+total_number_of_letters=len(all_letters)
+frequency_dictionary = dict()
+for letter, quantity in quantity_dictionary.items():
+    letter_frequency = quantity/total_number_of_letters
+    frequency_dictionary[letter] = letter_frequency
+
+for letter, frequency in frequency_dictionary.items():
+    print(f'{letter}: {frequency:.2f}')
+
